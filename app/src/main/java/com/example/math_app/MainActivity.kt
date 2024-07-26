cccc4b718c4c519fb5078b5dd64435a119a52352
Władysław Berdzik <161371933+TheGDProjekt@users.noreply.github.com>
2024-07-26 21:19:32 +0200

    Step 2

--- app/src/main/java/com/example/math_app/MainActivity.kt
+++ app/src/main/java/com/example/math_app/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.math_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val start: Button = findViewById(R.id.start)
+
+        start.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+            this.overridePendingTransition(R.anim.slide_in_left,
+                R.anim.slide_out_left);
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/math_app/QuizActivity.kt
@@ -0,0 +1,19 @@
+package com.example.math_app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class QuizActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+
+
+
+    }
+}
