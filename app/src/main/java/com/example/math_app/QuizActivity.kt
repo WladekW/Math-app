d81a559f8ea55641d8fc781dda20ea5e497146cb
Władysław Berdzik <161371933+TheGDProjekt@users.noreply.github.com>
2024-07-27 21:47:04 +0200

    Step 4.2

--- app/src/main/java/com/example/math_app/QuizActivity.kt
+++ app/src/main/java/com/example/math_app/QuizActivity.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
 
 class QuizActivity : AppCompatActivity() {
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +27,21 @@ class QuizActivity : AppCompatActivity() {
         val answer_c: Button = findViewById(R.id.answer_c)
         val answer_d: Button = findViewById(R.id.answer_d)
 
-        val level = 1
+        var level = 1
+
         val abcd = List(1) { Random.nextInt(1,5)}
 
+        var from = 1
+        var until = 10
+        val values_a = List(2) { Random.nextInt(from, until) }
+        val resula_a = values_a[0]
+        val result_b = values_a[1]
+        var values_c = resula_a + result_b
 
 
         if (level <= 2){
-            val values_a = List(2) { Random.nextInt(1, 10) }
-            val resula_a = values_a[0]
-            val result_b = values_a[1]
-            val values_c = resula_a + result_b
+            from = 1
+            until = 10
 
             if (abcd[0] == 1){
                 answer_a.setText("$values_c")
@@ -58,16 +66,46 @@ class QuizActivity : AppCompatActivity() {
             }
 
 
-            example_text.setText("$resula_a + $result_b =")
-            level_text.setText("Lv.$level")
-
 
         }
 
+        example_text.setText("$resula_a + $result_b =")
+        level_text.setText("Lv.$level")
 
-
-
+        answer_a.setOnClickListener {
+            if (abcd[0] == 1){
+                level += 1
+            }else{
+                level -= 1
+            }
+            level_text.setText("Lv.$level")
+        }
+        answer_b.setOnClickListener {
+            if (abcd[0] == 2){
+                level += 1
+            }else{
+                level -= 1
+            }
+            level_text.setText("Lv.$level")
+        }
+        answer_c.setOnClickListener {
+            if (abcd[0] == 3){
+                level += 1
+            }else{
+                level -= 1
+            }
+            level_text.setText("Lv.$level")
+        }
+        answer_d.setOnClickListener {
+            if (abcd[0] == 4){
+                level += 1
+            }else{
+                level -= 1
+            }
+            level_text.setText("Lv.$level")
+        }
 
 
     }
+
 }
